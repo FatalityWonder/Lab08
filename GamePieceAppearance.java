47aff4c6340a29a7cdb2b21f864c5a9e0d9960f9
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-03-14 13:23:09 -0500

    add method stubs

--- GamePieceAppearance.java
+++ GamePieceAppearance.java
@@ -1,5 +1,24 @@
 
 public class GamePieceAppearance 
 {
-
+	private Color color;
+	private Shape shape;
+	
+	public GamePieceAppearance(Color color, Shape shape)
+	{
+		this.color = color;
+		this.shape = shape;
+	}
+	
+	public Color getColor()
+	{
+		
+		return null; //TODO Stubbed
+	}
+	
+	public Shape getShape()
+	{
+		
+		return null //TODO Stubbed
+	}
 }
