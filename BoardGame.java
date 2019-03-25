456a6bf055fcf201afbba7e669926eaea0b731f9
Christian McBane <christian.j.mcbane-1@ou.edu>
2019-03-24 19:22:23 -0500

    work on boardGame

--- BoardGame.java
+++ BoardGame.java
@@ -7,69 +7,124 @@ public class BoardGame
 	protected LinkedHashMap<String, GamePiece> playerPieces;
 	protected LinkedHashMap<String, Location> playerLocation;
 	
-	public boolean addPlayer(String playerNam, GamePiece gamePiece, Location intialLocation)
+	public boolean addPlayer(String playerNam, GamePiece gamePiece, Location initialLocation)
 	{
+		if (playerPieces.containsKey(playerNam) && playerLocation.containsKey(playerNam))
+		{
+			return false;
+		}
 		
-		return true; //TODO Stubbed
+		playerPieces.put(playerNam, gamePiece);
+		playerLocation.put(playerNam, initialLocation);
+		
+		return true;
 	}
 	
 	public GamePiece getPlayerGamePiece(String playerName)
 	{
-		
-		return null; //TODO Stubbed
+		return playerPieces.get(playerName);
 	}
 	
 	public String getPlayerWithGamePiece(GamePiece gamePiece)
 	{
+		String player = null;
 		
-		return null; //TODO Stubbed
+		for (String name : playerPieces.keySet())
+		{
+			if (gamePiece.equals(playerPieces.get(name)))
+			{
+				player = name;
+				break;
+			}
+		}
+		
+		return player;
 	}
 	
 	public void movePlayer(String playerName, Location newLocation)
 	{
-		//TODO Complete
+		playerLocation.put(playerName, newLocation);
 	}
 	
 	public String[] moveTwoPlayers(String[] playerNames, Location[] newLocations)
 	{
+		GamePiece first = GamePiece.movesFirst(playerPieces.get(playerNames[0]), playerPieces.get(playerNames[1]));
+		GamePiece second;
+		Location firstloc;
+		Location secondloc;
+		String[] players = new String[2];
 		
-		return null; //TODO Stubbed
+		if (first.equals(playerPieces.get(playerNames[0])))
+		{
+			second = playerPieces.get(playerNames[1]);
+			secondloc = playerLocation.get(playerNames[1]);
+			firstloc = playerLocation.get(playerNames[0]);
+		}
+		else
+		{
+			second = playerPieces.get(playerNames[0]);
+			secondloc = playerLocation.get(playerNames[0]);
+			firstloc = playerLocation.get(playerNames[1]);
+		}
+		
+		players[0] = getPlayerWithGamePiece(first);
+		playerLocation.put(players[0], newLocations[0]);
+		
+		players[1] = getPlayerWithGamePiece(first);
+		
+		
+		
+		return players;
 	}
 	
 	public Location getPlayersLocation(String player)
 	{
-		
-		return null; //TODO Stubbed
+		return playerLocation.get(player);
 	}
 	
 	public ArrayList<String> getPlayersAtLocation(Location loc)
 	{
+		ArrayList<String> players = new ArrayList<String>();
 		
-		return null; //TODO Stubbed
+		for (String name : playerLocation.keySet())
+		{
+			if (loc.equals(playerLocation.get(name)))
+			{
+				players.add(name);
+			}
+		}
+		
+		return players;
 	}
 	
 	public ArrayList<GamePiece> getGamePiecesAtLocation(Location loc)
 	{
+		ArrayList<GamePiece> pieces = new ArrayList<GamePiece>();
 		
-		return null; //TODO Stubbed
+		for (String name : playerLocation.keySet())
+		{
+			if (loc.equals(playerLocation.get(name)))
+			{
+				pieces.add(playerPieces.get(name));
+			}
+		}
+		
+		return pieces;
 	}
 	
 	public Set<String> getPlayers()
 	{
-		
-		return null; //TODO Stubbed
+		return playerPieces.keySet();
 	}
 	
 	public Set<Location> getPlayerLocations()
 	{
-		
-		return null; //TODO Stubbed
+		return (Set<Location>) playerLocation.values();
 	}
 	
 	public Set<GamePiece> getPlayerPieces()
 	{
-		
-		return null; //TODO Stubbed
+		return (Set<GamePiece>) playerPieces.values();
 	}
 	
 }
---
+++ ColorTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ColorTest 
+{
+
+	@Test
+	void testGetR() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetG() 
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetB() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
